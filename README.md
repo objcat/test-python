ded9268fce88c709b7a730c9bfefd785983f538c
objcat <objcat@163.com>
2019-11-29 15:55:31 +0800

    添加简书转github markdown转换工具

--- README.md
+++ README.md
@@ -10,9 +10,11 @@ spider 爬虫目录
     - maoyan 爬取猫眼电影top100
     - liulian 爬取1688榴莲(需要cookie自己登陆1688使用chrome去抓取)
 tools 工具类
-    - tool 存放一些自己写的工具
+    - tool 存放一些工具
 plugin 外挂目录
     - farm 农产伴侣v1.0(需要cookie自己登陆手机qq空间去抓)
 configs 配置文件目录
     - config 配置数据库
+skill 完成某些功能的代码
+    - jianshu_to_github 把简书markdown转换成github
 ``` 
---
+++ src/skill/jianshu_to_github/conver.py
@@ -0,0 +1,63 @@
+import os
+import re
+import requests
+
+path = u"/Users/objcat/markdown/objcat_blog_markdown/markdown/Python/[Python] import自定义类"
+
+"""
+将简书markdown转换成github
+主要功能为下载图片后替换markdown中的路径为绝对路径 
+
+使用方法
+                            img 存放图片
+建立文件夹结构 文章名(文件夹) 
+                            md 存放md
+                            
+单个文章: 使用 download_replace 方法来指定单个文章文件夹
+多个文章: 一个大文件夹包含多个文章文件夹目录 使用 batch 方法给定大目录即可
+
+"""
+
+
+# 下载图片 替换文章
+def download_replace(dir_path):
+    md_path = dir_path + "/md"
+    img_path = dir_path + "/img"
+    for filename in os.listdir(md_path):
+        md = md_path + "/" + filename
+        f = open(md)
+        content = f.readlines()
+        f.close()
+        f = open(md)
+        content2 = f.read()
+        f.close()
+        i = 1
+        for url in content:
+            if "https://upload-images" in url:
+                pattern = re.compile(r"]\((.*?)\)")
+                item = re.findall(pattern, url)
+                print(item[0])
+                result = requests.get(item[0])
+                with open(img_path + "/" + str(i) + ".png", "wb") as f2:
+                    f2.write(result.content)
+                content2 = content2.replace(url, "![](" + "../img/" + str(i) + ".png" + ")" + "\n")
+                i += 1
+        # 生成新文档
+        with open(md_path + "/new.md", "w") as f2:
+            f2.write(content2)
+
+
+def batch(root_path):
+    for filename in os.listdir(root_path):
+        target_path = root_path + "/" + filename
+        if os.path.isdir(target_path):
+            for filename2 in os.listdir(target_path):
+                target_path2 = target_path + "/" + filename2
+                if os.path.isdir(target_path2):
+                    print("开始处理: " + filename2)
+                    download_replace(target_path2)
+
+
+if __name__ == '__main__':
+    # download_replace(path)
+    batch("/Users/objcat/markdown/objcat_blog_markdown/markdown")
